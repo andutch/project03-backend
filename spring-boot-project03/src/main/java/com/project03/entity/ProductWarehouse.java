cf1f5d04142eb0ee94a2b15dffea275f6429de50
Artyom <artyom.dutchak@gmail.com>
2023-01-04 23:06:33 -0700

    added getters/setters. removed lambok dependency

--- spring-boot-project03/src/main/java/com/project03/entity/ProductWarehouse.java
+++ spring-boot-project03/src/main/java/com/project03/entity/ProductWarehouse.java
@@ -1,6 +1,6 @@
 package com.project03.entity;
 
-import lombok.Getter;
+//import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -9,19 +9,43 @@ import java.util.Set;
 @Entity
 @Table(name="product_warehouse")
 // @Data -- known bug
-@Getter
-@Setter
+//@Getter
+//@Setter
 public class ProductWarehouse {
 
-    @Id
+    public Long getId() {
+		return id;
+	}
+
+	public String getWarehouseName() {
+		return warehouseName;
+	}
+
+	public String getWarehouseGrid() {
+		return warehouseGrid;
+	}
+
+	public Set<Product> getProducts() {
+		return products;
+	}
+
+	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
 
     @Column(name = "warehouse_name")
     private String warehouseName;
+    
+    @Column(name = "warehouse_grid")
+    private String warehouseGrid;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "warehouse")
     private Set<Product> products;
 
+    
+    
+    
+    
+    
 }
