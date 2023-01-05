98952591ec02f9240ac3b6d56bb9f02fbdd2d6f8
Artyom <artyom.dutchak@gmail.com>
2023-01-05 12:06:40 -0700

    removed lombok

--- spring-boot-project03/src/main/java/com/project03/entity/ProductWarehouse.java
+++ spring-boot-project03/src/main/java/com/project03/entity/ProductWarehouse.java
@@ -1,7 +1,7 @@
 package com.project03.entity;
 
-import lombok.Getter;
-import lombok.Setter;
+//import lombok.Getter;
+//import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.Set;
@@ -9,19 +9,43 @@ import java.util.Set;
 @Entity
 @Table(name="product_warehouse")
 // @Data -- known bug
-@Getter
-@Setter
+//@Getter
+//@Setter
 public class ProductWarehouse {
 
-    @Id
+    public Long getId() {
+		return id;
+	}
+
+	public String getWarehouseName() {
+		return warehouseName;
+	}
+
+	public String getWarehouseGrid() {
+		return warehouseGrid;
+	}
+
+	public Set<Product> getProducts() {
+		return products;
+	}
+
+	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
 
     @Column(name = "warehouse_name")
     private String warehouseName;
+    
+    @Column(name = "warehouse_grid")
+    private String warehouseGrid;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "warehouse")
     private Set<Product> products;
 
+    
+    
+    
+    
+    
 }
